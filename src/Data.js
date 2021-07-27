252c9fe8cb8e005e527e03d552bb15c0a0b088bc
Khaiser Khanam <Khaiser Khanam>
2021-07-27 19:28:52 +0530

    my first commit reactproject

---
+++ src/Data.js
@@ -0,0 +1,27 @@
+const Data=[
+    {
+        id:1,
+        question:"What is React?",
+        answer:"React is Javascript library for building User Interfaces"
+    },{
+        id:2,
+        question:"Who invented React?",
+        answer:"Facebook engineer Jordan Walke invented React"
+    },
+    {
+        id:3,
+        question:"What is JSX?",
+        answer:"It allows us to write HTML inside JavaScript and place them in the DOM "
+    },
+    {
+        id:4,
+        question:"What are the features of React? ",
+        answer:"It uses the virtual DOM instead of the real DOM.It uses serverside rendering."
+    },
+    {
+        id:5,
+        question:"What are the features of React? ",
+        answer:"It uses the virtual DOM instead of the real DOM.It uses serverside rendering."
+    }
+]
+export default Data;
